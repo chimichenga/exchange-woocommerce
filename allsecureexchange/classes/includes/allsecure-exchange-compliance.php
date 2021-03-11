c58b13d563c488c13fa297398f53b02efd3db967
chimichenga <nemanjaza94@gmail.com>
2021-03-11 14:22:43 +0100

    Refactored and styled cards banner

--- allsecureexchange/classes/includes/allsecure-exchange-compliance.php
+++ allsecureexchange/classes/includes/allsecure-exchange-compliance.php
@@ -5,13 +5,32 @@
 		$selectedBanner = $selected_allsecure->get_selected_banner();
 		$selectedCards = $selected_allsecure->get_selected_cards();
 		$selectedBank = $selected_allsecure->get_merchant_bank();
-		if (strpos($selectedCards, 'VISA') !== false) { $visa =  '<img src="' . plugins_url(). '/allsecureexchange/assets/images/'.$selectedBanner.'/visa.svg">';} else $visa = '';
-		if (strpos($selectedCards, 'MASTERCARD') !== false) { $mastercard = '<img src="' . plugins_url(). '/allsecureexchange/assets/images/'.$selectedBanner.'/mastercard.svg">';} else $mastercard = '';
-		if (strpos($selectedCards, 'MAESTRO') !== false) { $maestro = '<img src="' . plugins_url(). '/allsecureexchange/assets/images/'.$selectedBanner.'/maestro.svg">';} else $maestro = '';
-		if (strpos($selectedCards, 'AMEX') !== false) {$amex = '<img src="' . plugins_url(). '/allsecureexchange/assets/images/'.$selectedBanner.'/amex.svg">';} else $amex = '';
-		if (strpos($selectedCards, 'DINERS') !== false) {$diners = '<img src="' . plugins_url(). '/allsecureexchange/assets/images/'.$selectedBanner.'/diners.svg">';} else $diners = '';
-		if (strpos($selectedCards, 'JCB') !== false) {$jcb = '<img src="' . plugins_url(). '/allsecureexchange/assets/images/'.$selectedBanner.'/jcb.svg">';} else $jcb = '';
-		if (strpos($selectedCards, 'DINA') !== false) {$dina = '<img src="' . plugins_url(). '/allsecureexchange/assets/images/'.$selectedBanner.'/dina.svg">';} else $dina = '';
+
+		$visa = $mastercard = $maestro = $amex = $diners = $jcb = $dina = '';
+		$image_url_prefix = plugins_url() . '/allsecureexchange/assets/images/' . $selectedBanner;
+
+		if (strpos($selectedCards, 'VISA') !== false) {
+			$visa =  '<img src="' . $image_url_prefix . '/visa.svg">';
+		}
+		if (strpos($selectedCards, 'MASTERCARD') !== false) {
+			$mastercard = '<img src="' . $image_url_prefix . '/mastercard.svg">';
+		}
+		if (strpos($selectedCards, 'MAESTRO') !== false) {
+			$maestro = '<img src="' . $image_url_prefix . '/maestro.svg">';
+		}
+		if (strpos($selectedCards, 'AMEX') !== false) {
+			$amex = '<img src="' . $image_url_prefix . '/amex.svg">';
+		}
+		if (strpos($selectedCards, 'DINERS') !== false) {
+			$diners = '<img src="' . $image_url_prefix . '/diners.svg">';
+		}
+		if (strpos($selectedCards, 'JCB') !== false) {
+			$jcb = '<img src="' . $image_url_prefix . '/jcb.svg">';
+		}
+		if (strpos($selectedCards, 'DINA') !== false) {
+			$dina = '<img src="' . $image_url_prefix . '/dina.svg">';
+		}
+
 		$allsecure  = '<a href="https://www.allsecure.rs" target="_new"><img src="' . plugins_url(). '/allsecureexchange/assets/images/'.$selectedBanner.'/allsecure.svg"></a>'; 
 		if ($selectedBank == 'hbm') {
 			$bankUrl = 'https://www.hipotekarnabanka.com/'; 
@@ -30,9 +49,17 @@
 		$vbv = '<img src="' . plugins_url(). '/allsecureexchange/assets/images/'.$selectedBanner.'/visa_secure.svg">';
 		$mcsc = '<img src="' . plugins_url(). '/allsecureexchange/assets/images/'.$selectedBanner.'/mc_idcheck.svg">';
 		$allsecure_cards = $visa.''.$mastercard.''.$maestro.''.$diners.''.$amex.''.$jcb.''.$dina ;
+
 		if ($selectedBanner !== 'none') {
-			$allsecure_banner = '<div id="allsecure_exchange_banner"><div class="allsecure">'.$allsecure.'</div><div class="allsecure_threeds">'.$vbv.' '.$mcsc.'</div><div class="allsecure_cards">'.$allsecure_cards.'</div></div>';
-			if ($selectedBank !== 'none') $allsecure_banner = '<div id="allsecure_exchange_banner"><div class="allsecure">'.$allsecure.'</div><div class="allsecure_threeds">'.$vbv.' '.$mcsc.'</div><div class="allsecure_cards">'.$allsecure_cards.'</div><div class="allsecure_bank">'.$bank.'</div></div>';
+			$banner_items = $allsecure.$vbv.$mcsc.$allsecure_cards;
+			if ($selectedBank !== 'none')
+				$banner_items .= $bank;
+
+			$allsecure_banner = '
+			<div id="allsecure_exchange_banner">
+				'.$banner_items.'
+			</div>';
+
 			wp_enqueue_style( 'allsecure_style', plugins_url(). '/allsecureexchange/assets/css/allsecure-exchange-style.css', array(), null );
 			echo  $allsecure_banner;
 		}
